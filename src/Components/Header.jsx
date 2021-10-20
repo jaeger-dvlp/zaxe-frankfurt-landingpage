1d01908151629090fccbdf839fe1a13a0285867e
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-20 11:47:10 +0300

    Edit Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="container p-0 m-0 mx-auto">
+      <div className="flex justify-start py-5">
+        <img
+          className="h-6"
+          src="https://zaxe.com/public/svg-main/zaxe-logo-black-transparent.svg"
+          alt="zaxe_logo"
+        />
+      </div>
+    </div>
+  );
+}
