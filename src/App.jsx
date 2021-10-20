6bf965c464e11f9955ab650f54c26d382b807042
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-20 14:07:12 +0300

    Import Context API && PrintersNav Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+import { ContextProvider } from './ContextApi';
 import Header from './Components/Header';
+import PrintersNav from './Components/PrintersNav';
+
+AOS.init();
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <ContextProvider value="null">
+      <div data-aos="fade-in" className="App">
+        <Header />
+        <PrintersNav />
+      </div>
+    </ContextProvider>
   );
 }
 
