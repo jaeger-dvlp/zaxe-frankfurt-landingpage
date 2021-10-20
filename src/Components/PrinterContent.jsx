06500cc6fe5db6d7a8353a593bfe4820574e479b
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-20 17:31:50 +0300

    Process content data

--- src/Components/PrinterContent.jsx
+++ src/Components/PrinterContent.jsx
@@ -13,12 +13,22 @@ export default function PrinterContent() {
           return (
             <div
               key={`${cont.categoryName}${selectedPrinter}`}
-              className="w-1/2"
+              className="w-1/2 p-3 min-h-0 flex flex-wrap"
               data-aos="fade-right"
               data-aos-easing="ease-in-out-back"
               data-aos-duration="700"
             >
-              <div className="categoryHeader text-4xl">{cont.categoryName}</div>
+              <div className="categoryHeader w-full text-4xl ">
+                {cont.categoryName}
+              </div>
+              <div
+                className="mt-1 w-96"
+                style={{ height: '2px', backgroundColor: '#009ade' }}
+              ></div>
+              <div
+                dangerouslySetInnerHTML={{ __html: cont.categoryContent }}
+                className="categoryContent w-full py-2 h-80 text-lg whitespace-pre-wrap"
+              ></div>
             </div>
           );
         }
@@ -27,7 +37,7 @@ export default function PrinterContent() {
   });
 
   return (
-    <div className="container mx-auto p-3 py-10">
+    <div className="container mx-auto  py-10">
       <div className="w-full flex justify-center align-middle p-0">
         {elm}
         <div className="w-1/2">Image</div>
