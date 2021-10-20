234c8fcbf1838a36a711ad4a83a72fa55a1c1896
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-20 11:49:27 +0300

    Import Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Header from './Components/Header';
+
 function App() {
-  return <div className="App">Zaxe</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
