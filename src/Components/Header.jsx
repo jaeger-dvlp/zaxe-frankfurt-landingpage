f81a5959a0e66a896ae17ca7d3abc9798aadafcc
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-20 11:49:28 +0300

    Change logo height

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Header() {
   return (
     <div className="container p-0 m-0 mx-auto">
-      <div className="flex justify-start py-5">
+      <div className="flex justify-start py-6">
         <img
-          className="h-6"
+          className="h-10"
           src="https://zaxe.com/public/svg-main/zaxe-logo-black-transparent.svg"
           alt="zaxe_logo"
         />
