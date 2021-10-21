66911799183ace499a1d606d6d6b7f9015e2d9a6
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-21 16:56:41 +0300

    Import Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Header from './Components/Header';
 import PrintersNav from './Components/PrintersNav';
 import PrinterContent from './Components/PrinterContent';
 import ContentNav from './Components/ContentNav';
+import ImageViewer from './Components/ImageViewer';
 
 AOS.init({
   mirror: true,
@@ -16,11 +17,12 @@ AOS.init({
 function App() {
   return (
     <ContextProvider value="null">
-      <div data-aos="fade-in">
+      <div data-aos="fade-in" className="px-20">
         <Header />
         <PrintersNav />
         <PrinterContent />
         <ContentNav />
+        <ImageViewer />
       </div>
     </ContextProvider>
   );
